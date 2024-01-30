63d83602cfbed6db0a0f39753b152d9fc59667c3
Moltas Hultin <git@hultin.io>
2024-01-30 10:10:22 +0100

    Update gitignore and main.hs

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,10 @@
-import SudokuLogic 
+import SudokuLogic
 import Test.QuickCheck
 
 -- Test function for checking that it works
 prop_reverse :: [Int] -> Bool
 prop_reverse xs = reverse (reverse xs) == xs
 
-
--- 
-main:: a -> a
-main x = null
+--
+main :: IO ()
+main = return ()
--- src/sudokulogic.hs
+++ src/sudokulogic.hs
@@ -1,5 +1,6 @@
 module SudokuLogic where
+
 import Test.QuickCheck
 
-
-
+prop_reverse2 :: [Int] -> Bool
+prop_reverse2 xs = reverse (reverse xs) == xs
