c3a6e022e2108791bd053fb4adb4a7eed93a31cf
Karl Kjellmer <karl.kjellmer@gmail.com>
2024-02-13 11:12:08 +0100

    Sudoku skelleton first draft
    Co-authored-by: victor.ebbesson@telia.com <victor.ebbesson@telia.com>

--- src/sudoku.hs
+++ src/sudoku.hs
@@ -1,5 +1,42 @@
 module Sudoku where
 
+-- | A representation of the values in a sudoku from 0-9
+data Value
 
-data Value 
-    
+-- | A representation of a sudoku
+data Sudoku
+
+-- | a position in a soduku
+type Position = (Int, Int)
+
+type Row = [Value]
+
+type Column = [Value]
+
+type Block = [Value]
+
+-- | Returns all rows
+rows :: Sudoku ->  [Row]
+rows = undefined
+
+-- | Returns all colums
+columns :: Sudoku ->  [Column]
+columns = undefined
+
+-- | Returns all blocks
+blocks :: Sudoku ->  [Block]
+blocks = undefined
+
+-- | Get the row that this position is included in
+rowFromPos :: Sudoku -> Position -> Row
+rowFromPos = undefined
+
+-- | Get the column that this position is included in
+colFromPos :: Sudoku -> Position -> Column
+colFromPos = undefined
+
+-- | Get the block that this position is included in
+blockFromPos :: Sudoku -> Position -> Block 
+blockFromPos = undefined
+
+
