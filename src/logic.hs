ea4be2d5185ad9c1d141a6185c3dd7c51a8c1d59
Karl Kjellmer <karl.kjellmer@gmail.com>
2024-02-16 10:16:35 +0100

    logic and lemma testes

--- src/logic.hs
+++ src/logic.hs
@@ -1,1 +1,50 @@
 module Logic where
+
+import Sudoku
+
+type VarT = Int -- This is strange and bad but might work for the purpose of freshValue :)) 
+type Term = VarT 
+data Domain = BLOCK | ROW | COLLUMN | VALUE
+    deriving (Eq,Show)
+data SudSem = SudVal
+    deriving (Eq,Show)
+type Enviorment = ([(VarT, Domain)], Sudoku)
+
+freshValue :: Domain -> Enviorment -> Enviorment
+freshValue dom (env, sud) = (env ++ (name, dom), sud)
+    where name = last env + 1
+
+
+evalT :: Term -> Enviorment -> SudSem
+evalT t env = case lookup t env of 
+                Nothing -> error "Not in enviorment"
+                Just d -> d
+
+type PSym = String
+data FOL  =  Implies FOL FOL | And FOL FOL | Or FOL FOL | Not FOL
+          |  FORALL VarT Domain FOL    |  EXISTS VarT Domain FOL
+          |  PName PSym [Term]    |  Equal  Term  Term
+    deriving Show
+{-
+--eval0 :: PSym -> [SudSem] -> Bool
+--eval0 "Equal"     [t1,t2]  =  t1 == t2
+--eval0 "LessThan"  [t1,t2]  =  t1 < t2
+--eval0 "Positive"  [t1]     =  t1 > 0
+-}
+eval :: FOL -> Enviorment -> Bool
+eval formula env = ev formula
+  where  ev (PName n args)  = error "TODO"--eval0 n (map (flip evalT env) args)
+         ev (Equal a b)     = evalT a env == evalT b env
+         ev (And p q)       = ev p  &&  ev q
+         ev (Or  p q)       = ev p  ||  ev q
+         ev (FORALL v BLOCK p)      = undefined
+         ev (FORALL v ROW p)        = undefined
+         ev (FORALL v COLLUMN p)    = undefined
+         ev (FORALL v VALUE p)      = undefined
+         ev (EXISTS v BLOCK p)      = undefined
+         ev (EXISTS v ROW p)        = undefined
+         ev (EXISTS v COLLUMN p)    = undefined
+         ev (EXISTS v VALUE p)      = undefined
+
+forall :: FOL -> Enviorment -> Bool
+forall (FORALL v BLOCK p) (vs, sud) = and (eval p) [FORALL freshValue VALUE | b <- blocks sud]
--- src/sudokulogic.hs
+++ src/sudokulogic.hs
@@ -1,6 +1,43 @@
 module SudokuLogic where
 
-import Test.QuickCheck
+import Sudoku
+{-
+data Either a b = Left a | Right b
 
-prop_reverse2 :: [Int] -> Bool
-prop_reverse2 xs = reverse (reverse xs) == xs
+-}
+
+lemmaLastInRow :: Sudoku -> Position -> Bool
+lemmaLastInRow s pos = lastFreeCellInSection s pos (rowFromPos s pos)
+
+lemmaLastInCol :: Sudoku -> Position -> Bool
+lemmaLastInCol s pos = lastFreeCellInSection s pos (colFromPos s pos)
+
+lemmaLastInBlock :: Sudoku -> Position -> Bool
+lemmaLastInBlock s pos = lastFreeCellInSection s pos (blockFromPos s pos)
+
+-- | if a section only has one empty cell, then that cell must be filled with the only possible value
+lastFreeCellInSection :: Sudoku -> Position -> Section -> Bool
+lastFreeCellInSection s pos sec = oneCellInSectionV s sec && not (isFilled $ valFromPos s pos) 
+
+
+missingValues :: [Value] -> [Value]
+missingValues vs = [Filled x | x <- [One .. Nine], Filled x `notElem` vs]
+
+oneCellInSection :: Sudoku -> [(Int, Int)] -> Bool
+oneCellInSection s vs = length (filter (isFilled.valFromPos s) vs) == 8 
+
+
+oneCellInSectionV :: Sudoku -> [Value] -> Bool
+oneCellInSectionV s vs = length (filter isFilled vs) == 8 
+
+emptyPositions :: Sudoku -> [(Int, Int)]
+emptyPositions s = filter (not . isFilled . valFromPos s) positions
+    where
+    positions :: [(Int, Int)]
+    positions = [(x,y) | x <- [1..9], y <- [1..9]] 
+
+
+
+isFilled :: Value -> Bool
+isFilled (Filled _) = True
+isFilled _ = False
