d084f8bd59f13fc1384e8eb6e6b5e10ce00cbb55
Karl Kjellmer <karl.kjellmer@gmail.com>
2024-02-16 10:00:55 +0100

    Solver and Soduku skelleton
    Co-authored-by: victor.ebbesson@telia.com <victor.ebbesson@telia.com>

--- src/solver.hs
+++ src/solver.hs
@@ -1,1 +1,18 @@
 module Solver where
+
+import Sudoku
+
+data Step = LastFreeCell Position | LastRemainingCell Position
+    deriving (Eq,Show) -- | Then add more step types
+
+type Soulution = [Step]
+
+-- | A solver for sudoku
+
+solve :: Sudoku -> Soulution
+solve s = error "Not implemented"
+
+-- | next step in solving the sudoku
+nextStep :: Sudoku -> Step
+nextStep s = error "Not implemented"
+
