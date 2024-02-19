04f22518ef240d0593528a00b44c88714060ca21
Karl Kjellmer <karl.kjellmer@gmail.com>
2024-02-19 16:11:39 +0100

    Skelleton complete
    Co-authored-by: victor.ebbesson@telia.com <victor.ebbesson@telia.com>

--- src/logic.hs
+++ src/logic.hs
@@ -1,1 +1,10 @@
 module Logic where
+
+import Sudoku
+
+data FOL = And FOL FOL | Or FOL FOL | Not FOL | Implies FOL FOL | FORALL String FOL | EXISTS String FOL | True | False
+    deriving (Eq, Show)
+
+check :: FOL -> Bool
+check = error "Not implemented"
+
