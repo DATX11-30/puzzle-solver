9c73075424f59ecff9d7f09a73fd146d02a3a942
Karl Kjellmer <karl.kjellmer@gmail.com>
2024-02-20 11:40:07 +0100

    cleanup

--- src/sudokulogic.hs
+++ src/sudokulogic.hs
@@ -17,26 +17,10 @@ lemmaLastInBlock s pos = lastFreeCellInSection s pos (blockFromPos s pos)
 
 -- | if a section only has one empty cell, then that cell must be filled with the only possible value
 lastFreeCellInSection :: Sudoku -> Position -> Section -> Bool
-lastFreeCellInSection s pos sec = oneCellInSectionV s sec && not (isFilled $ valFromPos s pos) 
+lastFreeCellInSection s pos sec = oneCellInSection s sec && not (isFilled $ valFromPos s pos) 
 
-
-missingValues :: [Value] -> [Value]
-missingValues vs = [Filled x | x <- [One .. Nine], Filled x `notElem` vs]
-
-oneCellInSection :: Sudoku -> [(Int, Int)] -> Bool
-oneCellInSection s vs = length (filter (isFilled.valFromPos s) vs) == 8 
-
-
-oneCellInSectionV :: Sudoku -> [Value] -> Bool
-oneCellInSectionV s vs = length (filter isFilled vs) == 8 
-
-emptyPositions :: Sudoku -> [(Int, Int)]
-emptyPositions s = filter (not . isFilled . valFromPos s) positions
-    where
-    positions :: [(Int, Int)]
-    positions = [(x,y) | x <- [1..9], y <- [1..9]] 
-
-
+oneCellInSection :: Sudoku -> [Value] -> Bool
+oneCellInSection s vs = length (filter isFilled vs) == 8 
 
 isFilled :: Value -> Bool
 isFilled (Filled _) = True
