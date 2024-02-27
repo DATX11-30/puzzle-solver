eb223b41466871fd01de5dcca41db2546f4fa042
Karl Kjellmer <karl.kjellmer@gmail.com>
2024-02-27 11:43:05 +0100

    Added lemma Single candidate, And some more test sudokus. Tested and works

--- src/sudokulogic.hs
+++ src/sudokulogic.hs
@@ -2,7 +2,7 @@ module SudokuLogic where
 
 import Sudoku
 import Sudokus
-
+import Data.List
 
 type Lemma = Sudoku -> Position -> Bool
 
@@ -29,10 +29,53 @@ lemmaLastCellInColumn s pos = lastFreeCellInSection s pos (colFromPos s pos)
 lastFreeCellInSection :: Sudoku -> Position -> Section -> Bool
 lastFreeCellInSection s pos sec = oneCellInSection s sec && not (isFilled $ valFromPos s pos) 
 
+-- | A lemma that checks if a value is the only possible value for a cell
+lemmaSingleCandidate :: Sudoku -> Position -> Bool
+lemmaSingleCandidate sud pos = case candidates of
+                                [x] -> True
+                                _ -> False
+    where 
+        candidates = getCandidates sud pos
+
 prop_lastCell :: Position -> Bool
 prop_lastCell (row, col) = lemmaLastCellInRow (fillCell illegalSudoku (row, col) Empty) (row, col) && 
                            lemmaLastCellInColumn (fillCell illegalSudoku (row, col) Empty) (row, col) && 
                            lemmaLastCellInBlock (fillCell illegalSudoku (row, col) Empty) (row, col)
+
+-- | Returns all possible candidates 
+getCandidates :: Sudoku -> Position -> [SudVal]
+getCandidates s pos = candidates
+    where 
+        candidates = [x | x <- [One ..], not (elem (Filled x) occupiedVals)]
+        sections = sectionsFromPos s pos
+        occupiedVals = values ++ map Filled pairs
+        pairs = nub $ concat $ map notePairs sections 
+        values = nub $ concat sections 
+
+-- | Testes whether lemma singele candidate is valid
+prop_singleCandidate :: Position -> Bool
+prop_singleCandidate (row, col) = lemmaSingleCandidate (fillCell legalSudoku (row, col) Empty) (row, col) && 
+                                 not (lemmaSingleCandidate legalSudoku (row, col))
+
+
+--------------------------------------------------------------------------------------------------------
+-- Helper functions
+
+-- | If the section contains two cells witch include a note with the same valus, then return those values
+notePairs :: Section -> [SudVal]
+notePairs sec = nub $ concat pairs
+    where
+        notes = filter isNote sec
+        pairs = [x | (Note x) <- notes, (Note y) <- (delete (Note x) notes), x === y]
+
+-- | Returns true if two lists are set-equal
+(===) :: Eq a => [a] -> [a] -> Bool
+(===) xs ys = xs \\ ys == []
+
+isNote :: Value -> Bool
+isNote (Note _) = True
+isNote _ = False
+
 
 testSud :: Sudoku
 testSud = illegalSudoku 
