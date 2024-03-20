d7b2facf9f01898b6d16ced073b314d8cebd136b
Victor Ebbesson <victoreb@chalmers.se>
2024-03-20 11:08:09 +0100

    Cleaning

--- src/optimizer.hs
+++ src/optimizer.hs
@@ -1,5 +1,4 @@
 module Optimizer where
-
 import Sudoku
 import Sudokus 
 import SudokuLogic
@@ -14,8 +13,6 @@ Functions for prioritating which positions to check first
 -- | Creating the final list of positions by order of most used number and most filled block
 finalOrderOfPosition :: Sudoku -> [Position]
 finalOrderOfPosition sud = nub (concat [getAllPositionForValue sud sudval|sudval <- (sudValOrder (findOccuranceOfNumbers sud))])
-
--- [(0,0),(0,1),(1,0),(1,1),(2,0),(2,1),(3,3),(3,4),(3,5),(5,3),(5,4),(5,5),(3,6),(3,7),(3,8),(5,6),(5,7),(5,8),(6,0),(6,1),(7,0),(7,1),(8,0),(8,1),(0,3),(0,4),(0,5),(1,3),(1,4),(1,5),(2,3),(2,4),(2,5),(0,6),(0,7),(0,8),(1,6),(1,7),(1,8),(2,6),(2,7),(2,8),(6,3),(6,4),(6,5),(7,3),(7,4),(7,5),(8,3),(8,4),(8,5),(6,6),(6,7),(6,8),(7,6),(7,7),(7,8),(8,6),(8,7),(8,8)]
 
 -- | Returning all positions in each block  
 blocksPos :: [[Position]]
