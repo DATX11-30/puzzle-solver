df6e35a6387809e961a823c5d9833da39dd1b529
Moltas Hultin <git@hultin.io>
2024-04-09 17:09:48 +0200

    Start work on frontend

---
+++ server/app/[filename]/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { getSolution } from "@/lib/getSolution";
+import { useParams } from "next/navigation";
+import React, { useEffect } from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  const { filename } = useParams();
+  const [partialSuds, setPartialSuds] = React.useState<string[]>([]);
+  const [techniques, setTechniques] = React.useState<string[]>([]);
+
+  useEffect(() => {
+    getSolution(filename as string).then((solution) => {
+      setPartialSuds(solution.partialSuds);
+      setTechniques(solution.techniques);
+
+      console.log(solution);
+      
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Techniques</h1>
+      {techniques.map((technique, i) => {
+        return <div key={i}>{technique}</div>;
+      })}
+      {partialSuds.map((partialSud, i) => {
+        return <div key={i}>{partialSud}</div>;
+      })}
+    </div>
+  );
+};
+
+export default page;
--- src/main.hs
+++ src/main.hs
@@ -1,17 +1,50 @@
---import Test.QuickCheck
+import Data.List
 import IO
 import Logic
+import Optimizer
 import Solver
 import Sudoku
 import SudokuLogic
 import Sudokus
-import Optimizer
-import Data.List
+import System.Directory (doesFileExist)
+import System.Environment
 
--- Test function for checking that it works
-prop_reverse :: [Int] -> Bool
-prop_reverse xs = reverse (reverse xs) == xs
-
---
 main :: IO ()
-main = return ()
+main = do
+  args <- getArgs
+
+  if length args == 1
+    then do
+      let filePath :: FilePath = head args
+      let solFilePath = take (length filePath - 4) filePath ++ "_sol.txt"
+      fileExists <- doesFileExist solFilePath
+      if fileExists
+        then do
+          content <- readFile solFilePath
+          print content
+        else do
+          sud <- readSudoku filePath
+          let solution = fst (generateSolution sud 0)
+          writeFile solFilePath (show solution ++ "\n")
+          print (show solution)
+          applyStepAndWrite sud solution solFilePath
+    else
+      if length args == 2
+        then do
+          let sudString :: String = args !! 1
+          let sud = parseSudoku sudString
+          let solution = fst (generateSolution sud 0)
+          print (show solution)
+        else putStrLn "Usage: sudoku <sudoku_file_path> <sudoku_string>"
+  return ()
+
+
+applyStepAndWrite :: Sudoku -> Solution -> FilePath -> IO ()
+applyStepAndWrite sud [] _ = return ()
+applyStepAndWrite sud (x:xs) filePath = do
+  let sud' = placeValueFromStep sud x
+  let s = deParseSudoku sud'
+  appendFile filePath (s ++ "\n")
+  applyStepAndWrite sud' xs filePath
+  return ()
+ 
