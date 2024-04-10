5d2e73b3599f32c80d364801c755fef9fd2a5dbf
Krysset <maxhagman@hotmail.se>
2024-04-10 20:26:34 +0200

    Finished prel style

--- server/app/[filename]/page.tsx
+++ server/app/[filename]/page.tsx
@@ -7,31 +7,30 @@ import React, { useEffect } from "react";
 type Props = {};
 
 const page = (props: Props) => {
-  const { filename } = useParams();
-  const [partialSuds, setPartialSuds] = React.useState<string[]>([]);
-  const [techniques, setTechniques] = React.useState<string[]>([]);
+	const { filename } = useParams();
+	const [partialSuds, setPartialSuds] = React.useState<string[]>([]);
+	const [techniques, setTechniques] = React.useState<string[]>([]);
 
-  useEffect(() => {
-    getSolution(filename as string).then((solution) => {
-      setPartialSuds(solution.partialSuds);
-      setTechniques(solution.techniques);
+	useEffect(() => {
+		getSolution(filename as string).then((solution) => {
+			setPartialSuds(solution.partialSuds);
+			setTechniques(solution.techniques);
 
-      console.log(solution);
-      
-    });
-  }, []);
+			console.log(solution);
+		});
+	}, []);
 
-  return (
-    <div>
-      <h1>Techniques</h1>
-      {techniques.map((technique, i) => {
-        return <div key={i}>{technique}</div>;
-      })}
-      {partialSuds.map((partialSud, i) => {
-        return <div key={i}>{partialSud}</div>;
-      })}
-    </div>
-  );
+	return (
+		<div>
+			<h1>Techniques</h1>
+			{techniques.map((technique, i) => {
+				return <div key={i}>{technique}</div>;
+			})}
+			{partialSuds.map((partialSud, i) => {
+				return <div key={i}>{partialSud}</div>;
+			})}
+		</div>
+	);
 };
 
 export default page;
--- server/lib/getSolution.ts
+++ server/lib/getSolution.ts
@@ -2,32 +2,35 @@
 import { exec } from "child_process";
 import { cwd } from "process";
 
-
-async function getSolutionFromFile(fileName: String): Promise<String> {
-  return new Promise((resolve, reject) => {
-    exec("./out " + "./sudokus/" +fileName + '.txt', { cwd: cwd() }, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-      }
-      if (stderr) {
-        reject(stderr);
-      }
-      resolve(stdout);
-    });
-  });
+export async function getSolutionFromFile(fileName: string): Promise<string> {
+	return new Promise((resolve, reject) => {
+		exec("./out " + "./sudokus/" + fileName + ".txt", { cwd: cwd() }, (error, stdout, stderr) => {
+			if (error) {
+				reject(error);
+			}
+			if (stderr) {
+				reject(stderr);
+			}
+			resolve(stdout);
+		});
+	});
 }
 
-export async function getSolution(fileName: String): Promise<{partialSuds: String[], techniques: String[]}> {
-  const solution = await getSolutionFromFile(fileName);
-  const solutionArray = solution.split("\\n");
-
-  solutionArray.pop();
-
-  const partialSuds = solutionArray.splice(1);
-
-  const techniques = ['Not implemented'];
-
-  return {partialSuds, techniques};
+export async function getAllSudokus(): Promise<string> {
+	throw new Error("Not implemented");
 }
 
+export async function getSolution(
+	fileName: string
+): Promise<{ partialSuds: string[]; techniques: string[] }> {
+	const solution = await getSolutionFromFile(fileName);
+	const solutionArray = solution.split("\\n");
 
+	solutionArray.pop();
+
+	const partialSuds = solutionArray.splice(1);
+
+	const techniques = ["Not implemented"];
+
+	return { partialSuds, techniques };
+}
