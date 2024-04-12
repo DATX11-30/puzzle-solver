c5ef9e1149671a04ce04d4ac0906b0df7e5149f4
Moltas Hultin <git@hultin.io>
2024-04-12 15:51:40 +0200

    Continued work on frontend

--- server/lib/getSolution.ts
+++ server/lib/getSolution.ts
@@ -3,34 +3,40 @@ import { exec } from "child_process";
 import { cwd } from "process";
 
 export async function getSolutionFromFile(fileName: string): Promise<string> {
-	return new Promise((resolve, reject) => {
-		exec("./out " + "./sudokus/" + fileName + ".txt", { cwd: cwd() }, (error, stdout, stderr) => {
-			if (error) {
-				reject(error);
-			}
-			if (stderr) {
-				reject(stderr);
-			}
-			resolve(stdout);
-		});
-	});
+  return new Promise((resolve, reject) => {
+    exec(
+      "./out " + "./sudokus/" + fileName + ".txt",
+      { cwd: cwd() },
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        }
+        if (stderr) {
+          reject(stderr);
+        }
+        resolve(stdout);
+      }
+    );
+  });
 }
 
 export async function getAllSudokus(): Promise<string> {
-	throw new Error("Not implemented");
+  throw new Error("Not implemented");
 }
 
 export async function getSolution(
-	fileName: string
+  fileName: string
 ): Promise<{ partialSuds: string[]; techniques: string[] }> {
-	const solution = await getSolutionFromFile(fileName);
-	const solutionArray = solution.split("\\n");
+  const solution = await getSolutionFromFile(fileName);
 
-	solutionArray.pop();
+  const lines = solution.split("\\n");
+  const partialSudsIndex = lines.findIndex(
+    (line) => line === "PartialSudokus:"
+  );
+  const techniques = lines
+    .slice(1, partialSudsIndex - 1)
+    .map((line) => line.replaceAll("\\", "").replaceAll('"', ""));
+  const partialSuds = lines.slice(partialSudsIndex + 1, lines.length - 1);
 
-	const partialSuds = solutionArray.splice(1);
-
-	const techniques = ["Not implemented"];
-
-	return { partialSuds, techniques };
+  return { partialSuds, techniques };
 }
