0670a476e8162c14081b0d7e5d5fb4f0e5bb2466
Moltas Hultin <git@hultin.io>
2024-04-12 15:51:40 +0200

    Continued work on frontend

--- server/app/[filename]/page.tsx
+++ server/app/[filename]/page.tsx
@@ -1,36 +1,54 @@
 "use client";
 
+import Sudoku from "@/components/sudoku/sudoku";
 import { getSolution } from "@/lib/getSolution";
 import { useParams } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {};
 
 const page = (props: Props) => {
-	const { filename } = useParams();
-	const [partialSuds, setPartialSuds] = React.useState<string[]>([]);
-	const [techniques, setTechniques] = React.useState<string[]>([]);
+  const { filename } = useParams();
+  const [partialSuds, setPartialSuds] = useState<string[] | null>(null);
+  const [techniques, setTechniques] = useState<string[] | null>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-	useEffect(() => {
-		getSolution(filename as string).then((solution) => {
-			setPartialSuds(solution.partialSuds);
-			setTechniques(solution.techniques);
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key == "ArrowRight") {
+      setCurrentIndex((prev) => prev + 1);
+    } else if (event.key == "ArrowLeft") {
+      setCurrentIndex((prev) => prev - 1);
+    }
+  };
 
-			console.log(solution);
-		});
-	}, []);
+  useEffect(() => {
+    getSolution(filename as string).then((solution) => {
+		console.log(solution);
 
-	return (
-		<div>
-			<h1>Techniques</h1>
-			{techniques.map((technique, i) => {
-				return <div key={i}>{technique}</div>;
-			})}
-			{partialSuds.map((partialSud, i) => {
-				return <div key={i}>{partialSud}</div>;
-			})}
-		</div>
-	);
+      setPartialSuds(solution.partialSuds);
+	  setTechniques(solution.techniques);
+    });
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  if (!partialSuds || !techniques) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <p>{currentIndex + 1} / {partialSuds.length}</p>
+	  <Sudoku sudoku={partialSuds[currentIndex]} />
+	  <p>{techniques[currentIndex]}</p>
+    </div>
+  );
 };
 
 export default page;
