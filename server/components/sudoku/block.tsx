87f47767a432854e5d430788d74ef61fd16cd8f1
Krysset <maxhagman@hotmail.se>
2024-04-19 13:42:32 +0200

    highlight cell

--- server/components/sudoku/block.tsx
+++ server/components/sudoku/block.tsx
@@ -1,27 +1,33 @@
 import Cell from "./cell";
 
-export default function Block({ values }: { values?: (string | string[])[] }) {
-  let value: string[] = new Array(9).fill("0");
+export default function Block({
+	values,
+	highlight
+}: {
+	values?: (string | string[])[];
+	highlight?: number;
+}) {
+	let value: string[] = new Array(9).fill("0");
 
-  if (!values) {
-    return (
-      <div className="block">
-        {value.map((v, i) => {
-          return <Cell key={i} value={v} />;
-        })}
-      </div>
-    );
-  }
+	if (!values) {
+		return (
+			<div className="block">
+				{value.map((v, i) => {
+					return <Cell key={i} value={v} />;
+				})}
+			</div>
+		);
+	}
 
-  return (
-    <div className="block">
-      {values.map((v, i) => {
-        if (Array.isArray(v)) {
-          value = v;
-          return <Cell key={i} notes={v} />;
-        }
-        return <Cell key={i} value={v} />;
-      })}
-    </div>
-  );
+	return (
+		<div className="block">
+			{values.map((v, i) => {
+				if (Array.isArray(v)) {
+					value = v;
+					return <Cell key={i} notes={v} highlight={highlight === i} />;
+				}
+				return <Cell key={i} value={v} />;
+			})}
+		</div>
+	);
 }
--- server/components/sudoku/sudoku.tsx
+++ server/components/sudoku/sudoku.tsx
@@ -1,38 +1,53 @@
 import Block from "./block";
 import "./sudoku.css";
 
-export default function Sudoku({ sudoku }: { sudoku?: string }) {
-
-	if(!sudoku) {
-		return <div className="sudoku overflow-auto">
-			{[...Array(9)].map((_, i) => {
-				return <Block key={i} />;
-			})}
-		</div>
+export default function Sudoku({
+	sudoku,
+	highlight
+}: {
+	sudoku?: string;
+	highlight?: { row: number; col: number };
+}) {
+	if (!sudoku) {
+		return (
+			<div className="sudoku overflow-auto">
+				{[...Array(9)].map((_, i) => {
+					return <Block key={i} />;
+				})}
+			</div>
+		);
 	}
 
-  	const sudokuValues: (string | string[])[] = [];
+	const sudokuValues: (string | string[])[] = [];
 
 	const sudokuArray = sudoku?.split("");
-    for (let i = 0; i < sudokuArray?.length; i++) {
-      if (!isNaN(+sudokuArray[i] )) {
-        sudokuValues.push(sudokuArray[i]);
-      } else if (sudokuArray[i] === "[") {
-        sudokuValues.push([sudokuArray[i + 1], sudokuArray[i + 3]]);
-        i += 3;
-      }
-    }
+	for (let i = 0; i < sudokuArray?.length; i++) {
+		if (!isNaN(+sudokuArray[i])) {
+			sudokuValues.push(sudokuArray[i]);
+		} else if (sudokuArray[i] === "[") {
+			sudokuValues.push([sudokuArray[i + 1], sudokuArray[i + 3]]);
+			i += 3;
+		}
+	}
 
-    console.log(sudokuValues);
-  
+	console.log(sudokuValues);
 
-  return (
-    <div className="sudoku overflow-auto">
-      {[...Array(9)].map((_, i) => {
-          return (
-            <Block key={i} values={sudokuValues.slice(i * 9, (i + 1) * 9)} />
-          );
-      })}
-    </div>
-  );  
+	return (
+		<div className="sudoku overflow-auto">
+			{[...Array(9)].map((_, i) => {
+				let highlightIndex = undefined;
+				if (highlight) {
+					highlightIndex =
+						highlight.row === Math.floor(i / 3) && highlight.col === i % 3 ? 4 : undefined;
+				}
+				return (
+					<Block
+						key={i}
+						values={sudokuValues.slice(i * 9, (i + 1) * 9)}
+						highlight={highlightIndex}
+					/>
+				);
+			})}
+		</div>
+	);
 }
